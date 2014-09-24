2e82114fa01733be7a487c0ebff5da0bb2cf4092
Jo <gelin_jo@hotmail.com>
2014-09-24 18:03:44 +0200

    firt init

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name verdiccijsApp
+ * @description
+ * # verdiccijsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('verdiccijsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name verdiccijsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the verdiccijsApp
+ */
+angular.module('verdiccijsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
